06cd3988b28e9cfb38798176ba1cbd0971d1549d
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-07-28 21:08:24 -0500

    Start project

---
+++ src/main/java/com/alex/core/model/Customer.java
@@ -0,0 +1,15 @@
+package com.alex.core.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+	String name;
+	String lastName;
+	String firstName;
+
+}
