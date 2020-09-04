15d96885bd0e8480307976a39ffdbd27e945fc7c
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-09-03 19:53:38 -0500

    Add header values to kafka listener

--- spring-kafka-simple-consumer/src/main/java/com/core/service/ConsumerService.java
+++ spring-kafka-simple-consumer/src/main/java/com/core/service/ConsumerService.java
@@ -1,6 +1,9 @@
 package com.core.service;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
@@ -10,8 +13,19 @@ import lombok.extern.slf4j.Slf4j;
 public class ConsumerService {
 
 	@KafkaListener(topics = "test", groupId = "first")
-	public void consume(String message) {
-		log.info(String.format("Received the following message [ %s ]", message));
-		System.out.println("Received the following message: " + message);
+	public void consume(@Payload String message,
+			@Header(name = KafkaHeaders.RECEIVED_MESSAGE_KEY, required = false) String key,
+			@Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+			@Header(KafkaHeaders.RECEIVED_TOPIC) String topic, @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long ts) {
+
+		log.info(String.format("Payload [ %s ]", message));
+		log.info(String.format("Key [ %s ]", key));
+		log.info(String.format("Partition [ %s ]", partition));
+		log.info(String.format("Topic [ %s ]", topic));
+		log.info(String.format("Timestamp [ %s ]", ts));
+
+		System.out.println("Received: [" + key + ":" + message + "]");
+
 	}
+
 }
